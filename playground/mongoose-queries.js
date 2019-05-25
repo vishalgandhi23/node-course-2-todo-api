388f623bf80137b1cf0b0379fcf818cc23df70ac
Vishal Gandhi <vishalgandhi94@gmail.com>
2019-05-26 02:01:50 +0530

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,42 @@
+const{ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5ce6e845dbc768904339df05';
+
+// if(!ObjectID.isValid(id)){
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo',todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if(!todo){
+//         return console.log('Id not found.');
+//     }
+//     console.log('Todo by id',todo);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+User.findById(id).then((user) => {
+    if(!user){
+        return console.log('User not found');
+    }
+
+    console.log('User by id',user);
+}, (e) => {
+    console.log(e);
+});
